1fa3785504bbabd7061cff01103c619317daf156
iamIcarus <iamIcarus@users.noreply.github.com>
2025-02-05 19:56:22 +0200

    refactor: remove unused address from ton/core

--- tests/SkipperDynamicLock.spec.ts
+++ tests/SkipperDynamicLock.spec.ts
@@ -1,5 +1,5 @@
 import { Blockchain, SandboxContract, TreasuryContract } from '@ton/sandbox';
-import { address, beginCell, toNano } from '@ton/core';
+import { beginCell, toNano } from '@ton/core';
 import { Skipper } from '../wrappers/Skipper';
 import '@ton/test-utils';
 import { JettonMaster } from './JettonMaster';
@@ -7,7 +7,6 @@ import { JettonWallet } from './JettonWallet';
 import { JettonLock } from '../wrappers/Lock';
 import { Proposal } from '../wrappers/Proposal';
 import { EXIT_CODES, LOCK_INTERVAL, LOCK_MIN_INTERVAL ,  OP_CODES } from './constants';
-
 
 describe('Initializing proposals tests with dynamic lock', () => {
     let blockchain: Blockchain;
@@ -174,12 +173,322 @@ describe('Initializing proposals tests with dynamic lock', () => {
         await runCreateProposalTest(BigInt(5), LOCK_MIN_INTERVAL );
     });
 
-    it('should try to create proposal with lock_period less than the LOCK_INTERVAL ', async () => {
+    it('should create proposal with lock_period less than the LOCK_INTERVAL ', async () => {
         // create proposal that is less than the LOCK_INTERVAL but greater than the LOCK_MIN_INTERVAL
         await runCreateProposalTest(BigInt(LOCK_MIN_INTERVAL + 5), LOCK_MIN_INTERVAL + 5 );
     });
 
-    it('should try to create proposal with lock_period greater than the LOCK_INTERVAL ', async () => {
+    it('should create proposal with lock_period greater than the LOCK_INTERVAL ', async () => {
         await runCreateProposalTest(BigInt(LOCK_INTERVAL + 5), LOCK_INTERVAL + 5 );
     });
 });
+
+
+describe('Particibating in proposals tests with dynamic lock', () => {
+    let blockchain: Blockchain;
+    let deployer: SandboxContract<TreasuryContract>;
+    let user2: SandboxContract<TreasuryContract>;
+    let jetton_master: SandboxContract<JettonMaster>;
+    let jetton_wallet: SandboxContract<JettonWallet>;
+    let user2_jetton_wallet: SandboxContract<JettonWallet>;
+    let skipper: SandboxContract<Skipper>;
+    let lock: SandboxContract<JettonLock>;
+    let user2_lock: SandboxContract<JettonLock>;
+    let proposal: SandboxContract<Proposal>;
+
+    
+    beforeEach(async () => {
+        blockchain = await Blockchain.create();
+
+        // Set the current time
+        let startTime = Math.floor(Date.now() / 1000);
+        blockchain.now = startTime;
+
+        deployer = await blockchain.treasury('deployer');
+        user2 = await blockchain.treasury('user2');
+
+        jetton_master = blockchain.openContract(await JettonMaster.fromInit(deployer.address));
+        jetton_wallet = blockchain.openContract(await JettonWallet.fromInit(jetton_master.address, deployer.address));
+        user2_jetton_wallet = blockchain.openContract(await JettonWallet.fromInit(jetton_master.address, user2.address));
+
+        skipper = blockchain.openContract(await Skipper.fromInit(jetton_master.address));
+        lock = blockchain.openContract(await JettonLock.fromInit(deployer.address, jetton_master.address));
+        user2_lock = blockchain.openContract(await JettonLock.fromInit(user2.address, jetton_master.address));
+        proposal = blockchain.openContract(await Proposal.fromInit(skipper.address, 1n));
+
+
+        //Setup user1 (deployer) accounts 
+       //==========================================
+        await jetton_master.send(
+            deployer.getSender(),
+            {
+                value: toNano("0.05"),
+            },
+            {
+                $$type: 'JettonMint',
+                query_id: 0n,
+                amount: toNano('1337000'),
+                destination: deployer.address,
+            }
+        );
+        const jettonWalletData = await jetton_wallet.getGetWalletData();
+        expect(jettonWalletData.balance).toEqual(toNano('1337000'));      
+
+        await lock.send(
+            deployer.getSender(),
+            {
+                value: toNano('0.05')
+            },
+            {
+                $$type: 'Deploy',
+                queryId: 0n,
+            }
+        );        
+
+        await jetton_wallet.send(
+            deployer.getSender(),
+            {
+                value: toNano('0.1'),
+            },
+            {
+                $$type: 'JettonTransfer',
+                query_id: 0n,
+                amount: toNano("1337000"),
+                destination: lock.address,
+                custom_payload: null,
+                forward_payload: beginCell().asSlice(),
+                forward_ton_amount: toNano("0.05"),
+                response_destination: lock.address,
+            }
+        );
+
+        const lockData = await lock.getGetLockData();
+        expect(lockData.amount).toEqual(toNano('1337000'));        
+
+        //Setup user2 (deployer) accounts 
+        //==========================================
+        await jetton_master.send(
+            deployer.getSender(),
+            {
+                value: toNano("0.05"),
+            },
+            {
+                $$type: 'JettonMint',
+                query_id: 0n,
+                amount: toNano('1337000'),
+                destination: user2.address,
+            }
+        );
+        const jettonWalletUser2Data = await user2_jetton_wallet.getGetWalletData();
+        expect(jettonWalletUser2Data.balance).toEqual(toNano('1337000'));      
+
+        await user2_lock.send(
+            user2.getSender(),
+            {
+                value: toNano('0.05')
+            },
+            {
+                $$type: 'Deploy',
+                queryId: 0n,
+            }
+        );        
+
+        await user2_jetton_wallet.send(
+            user2.getSender(),
+            {
+                value: toNano('0.1'),
+            },
+            {
+                $$type: 'JettonTransfer',
+                query_id: 0n,
+                amount: toNano("1337000"),
+                destination: user2_lock.address,
+                custom_payload: null,
+                forward_payload: beginCell().asSlice(),
+                forward_ton_amount: toNano("0.05"),
+                response_destination: user2_lock.address,
+            }
+        );
+
+        const lockUser2Data = await user2_lock.getGetLockData();
+        expect(lockUser2Data.amount).toEqual(toNano('1337000'));
+
+        //Setup Skipper
+        //==========================================
+        const deployResult = await skipper.send(
+            deployer.getSender(),
+            {
+                value: toNano('0.05'),
+            },
+            {
+                $$type: 'Deploy',
+                queryId: 0n,
+            }
+        );
+        expect(deployResult.transactions).toHaveTransaction({
+            from: deployer.address,
+            to: skipper.address,
+            deploy: true,
+            success: true,
+        });
+
+
+        //Setup proposal with deployer as the intiator
+        //==========================================
+        var random = Math.floor(Math.random() * 900000);
+        const createProposalResult = await lock.send(
+            deployer.getSender(),
+            {
+                value: toNano("1")
+            },
+            {
+                $$type: 'SendProxyMessage',
+                to: skipper.address,
+                lock_period: BigInt(LOCK_INTERVAL),
+                payload: beginCell()
+                .storeUint(OP_CODES.RequestNewProposal, 32)
+                .storeAddress(deployer.address)                
+                .storeRef(
+                     beginCell()
+                    .storeUint(random, 256)
+                    .endCell())
+                .asCell(),
+            }
+        );
+
+        expect(createProposalResult.transactions).toHaveTransaction({
+            from: deployer.address,
+            to: lock.address,
+            success: true,
+            op: OP_CODES.SendProxyMessage,
+        });
+        expect(createProposalResult.transactions).toHaveTransaction({
+            from: lock.address,
+            to: skipper.address,
+            success: true,
+            op: OP_CODES.ProxyMessage,
+        });
+        expect(createProposalResult.transactions).toHaveTransaction({
+            from: skipper.address,
+            to: proposal.address,
+            success: true,
+            op: OP_CODES.InitProposal,
+        });
+        expect(createProposalResult.transactions).toHaveTransaction({
+            from: proposal.address,
+            // to: voter.address,
+            success: true,
+            op: OP_CODES.InitVoter,
+        });
+
+        // Check the proposal data
+        const proposalData = await proposal.getGetProposalData();
+        expect(proposalData?.receiver.toString()).toEqual(deployer.address.toString());
+
+        // Decode the body of ProposalData
+        const proposalDataSlice = proposalData!.body.beginParse();
+        const randomInt = proposalDataSlice.loadUint(256);
+        expect(randomInt).toEqual(random);
+
+        //check the expiry date for the proposal
+        const expiresAt = await proposal.getExpiresAt();
+        expect(expiresAt).toEqual(BigInt(blockchain.now + LOCK_INTERVAL));
+    });
+
+    async function runVoteProposalTest(lock_period: bigint | null): Promise<any> {
+        const voteProposalResult = await user2_lock.send(
+            user2.getSender(),
+            {
+                value: toNano("1")
+            },
+            {
+                $$type: 'SendProxyMessage',
+                to: skipper.address,
+                lock_period: lock_period,
+                payload: beginCell()
+                    .storeUint(OP_CODES.VoteForProposal, 32)
+                    .storeUint(1, 64)
+                    .storeUint(1, 1)
+                    .asCell(),
+            }
+        );
+        expect(voteProposalResult.transactions).toHaveTransaction({
+            from: user2.address,
+            to: user2_lock.address,
+            success: true,
+            op: OP_CODES.SendProxyMessage,
+        });
+        expect(voteProposalResult.transactions).toHaveTransaction({
+            from: user2_lock.address,
+            to: skipper.address,
+            success: true,
+            op: OP_CODES.ProxyMessage,
+        });
+
+        expect(voteProposalResult.transactions).toHaveTransaction({
+            from: skipper.address,
+            // to: voter.address,
+            success: true,
+            op: OP_CODES.UpdateVoterBalance,
+        });
+  
+        return voteProposalResult
+    }
+
+    it('should create proposal with the default LOCK_INTERVAL', async () => {
+        let voteProposalResult = await runVoteProposalTest(BigInt(LOCK_INTERVAL));
+
+        expect(voteProposalResult.transactions).toHaveTransaction({
+            // from: voter.address,
+            to: proposal.address,
+            success: true,
+            op: OP_CODES.UpdateVotes,
+        });      
+    });
+
+    it('should try to create proposal with lock_period 0 and revert to LOCK_INTERVAL', async () => {
+        let voteProposalResult =  await runVoteProposalTest(BigInt(0));
+
+        expect(voteProposalResult.transactions).toHaveTransaction({
+            // from: voter.address,
+            to: proposal.address,
+            success: true,
+            op: OP_CODES.UpdateVotes,
+        });      
+    });
+
+    it('should try to create proposal with lock_period null and revert to LOCK_INTERVAL', async () => {
+        let voteProposalResult =  await runVoteProposalTest(null);
+
+        expect(voteProposalResult.transactions).toHaveTransaction({
+            // from: voter.address,
+            to: proposal.address,
+            success: true,
+            op: OP_CODES.UpdateVotes,
+        });      
+    });
+
+    it('should try to create proposal with lock_period not sufficient for the proposal', async () => {
+        let voteProposalResult =  await runVoteProposalTest(5n);
+
+        expect(voteProposalResult.transactions).toHaveTransaction({
+            // from: voter.address,
+            to: proposal.address,
+            success: false,
+            op: OP_CODES.UpdateVotes,
+            exitCode: EXIT_CODES.UnlockDateInsufficient
+        });      
+    });
+    
+    it('should create proposal with lock_period that spans more than the proposal expiry', async () => {
+        let voteProposalResult =  await runVoteProposalTest(BigInt(LOCK_INTERVAL) + 5n);
+
+        expect(voteProposalResult.transactions).toHaveTransaction({
+            // from: voter.address,
+            to: proposal.address,
+            success: true,
+            op: OP_CODES.UpdateVotes,
+        });      
+    });
+});
+
